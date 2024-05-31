6bfb35c5e69e3086d887ebc512cb3f3bce7a439c
Abdulmalik <hackobansa@gmail.com>
2024-05-31 16:32:14 +0100

    updated marker

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 // Sample data to demonstrate functionality (replace with real data)
 const data = [
+        {
+        "POTEAU_ID_POT": 7444,
+        "POSITION_POP": 7,
+        "PANNEAU_ID_PAN": 13565,
+        "PANNEAU_ID_RPA": 2604,
+        "DESCRIPTION_RPA": "\\P 09h30-10h30 MER. 1 MARS AU 1 DEC.",
+        "CODE_RPA": "SU-ND",
+        "FLECHE_PAN": 0,
+        "TOPONYME_PAN": "Main Street",
+        "DESCRIPTION_CAT": "STATIONNEMENT",
+        "POTEAU_VERSION_POT": 0,
+        "DATE_CONCEPTION_POT": "1999-11-05",
+        "PAS_SUR_RUE": null,
+        "DESCRIPTION_REP": "Enlevé",
+        "DESCRIPTION_RTP": "1- Tige et base",
+        "Longitude": -73.571238,
+        "Latitude": 45.547944,
+        "NOM_ARROND": "Rosemont - La Petite-Patrie",
+        "Latrine": "Yes",
+        "Township": "Libore",
+        "Status": "No",
+        "Electricity": "No",
+        "Internet": "No",
+        "ConstructionMaterial": "Mix",
+        "Water": "No",
+        "Fencing": "No",
+        "Library": "No",
+        "ConnectionSpeed": "N/A",
+        "Boys": "N/A",
+        "TeachingStaff": "N/A",
+        "PhoneNumber": "N/A",
+        "Girls": "N/A",
+        "TotalStudents": "N/A",
+        "ComputerRoom": "No",
+        "MobileNetwork": "No"
+    },
     {
         "POTEAU_ID_POT": 7444,
         "POSITION_POP": 7,
@@ -17,7 +53,24 @@ const data = [
         "DESCRIPTION_RTP": "1- Tige et base",
         "Longitude": -73.571238,
         "Latitude": 45.547944,
-        "NOM_ARROND": "Rosemont - La Petite-Patrie"
+        "NOM_ARROND": "Rosemont - La Petite-Patrie",
+        "Latrine": "Yes",
+        "Township": "Libore",
+        "Status": "No",
+        "Electricity": "No",
+        "Internet": "No",
+        "ConstructionMaterial": "Mix",
+        "Water": "No",
+        "Fencing": "No",
+        "Library": "No",
+        "ConnectionSpeed": "N/A",
+        "Boys": "N/A",
+        "TeachingStaff": "N/A",
+        "PhoneNumber": "N/A",
+        "Girls": "N/A",
+        "TotalStudents": "N/A",
+        "ComputerRoom": "No",
+        "MobileNetwork": "No"
     },
     {
         "POTEAU_ID_POT": 59640,
@@ -36,7 +89,24 @@ const data = [
         "DESCRIPTION_RTP": "4- Poteau en bois",
         "Longitude": -73.587793,
         "Latitude": 45.474864,
-        "NOM_ARROND": "Le Sud-Ouest"
+        "NOM_ARROND": "Le Sud-Ouest",
+        "Latrine": "Yes",
+        "Township": "Libore",
+        "Status": "No",
+        "Electricity": "No",
+        "Internet": "No",
+        "ConstructionMaterial": "Mix",
+        "Water": "No",
+        "Fencing": "No",
+        "Library": "No",
+        "ConnectionSpeed": "N/A",
+        "Boys": "N/A",
+        "TeachingStaff": "N/A",
+        "PhoneNumber": "N/A",
+        "Girls": "N/A",
+        "TotalStudents": "N/A",
+        "ComputerRoom": "No",
+        "MobileNetwork": "No"
     },
     {
         "POTEAU_ID_POT": 62627,
@@ -55,7 +125,24 @@ const data = [
         "DESCRIPTION_RTP": "2- Tige et manchon",
         "Longitude": -73.576569,
         "Latitude": 45.499245,
-        "NOM_ARROND": "Ville-Marie"
+        "NOM_ARROND": "Ville-Marie",
+        "Latrine": "Yes",
+        "Township": "Libore",
+        "Status": "No",
+        "Electricity": "No",
+        "Internet": "No",
+        "ConstructionMaterial": "Mix",
+        "Water": "No",
+        "Fencing": "No",
+        "Library": "No",
+        "ConnectionSpeed": "N/A",
+        "Boys": "N/A",
+        "TeachingStaff": "N/A",
+        "PhoneNumber": "N/A",
+        "Girls": "N/A",
+        "TotalStudents": "N/A",
+        "ComputerRoom": "No",
+        "MobileNetwork": "No"
     },
     {
         "POTEAU_ID_POT": 60326,
@@ -74,7 +161,24 @@ const data = [
         "DESCRIPTION_RTP": "3- Fût",
         "Longitude": -73.58679,
         "Latitude": 45.628754,
-        "NOM_ARROND": "Rivière-des-Prairies - Pointe-aux-Trembles"
+        "NOM_ARROND": "Rivière-des-Prairies - Pointe-aux-Trembles",
+        "Latrine": "Yes",
+        "Township": "Libore",
+        "Status": "No",
+        "Electricity": "No",
+        "Internet": "No",
+        "ConstructionMaterial": "Mix",
+        "Water": "No",
+        "Fencing": "No",
+        "Library": "No",
+        "ConnectionSpeed": "N/A",
+        "Boys": "N/A",
+        "TeachingStaff": "N/A",
+        "PhoneNumber": "N/A",
+        "Girls": "N/A",
+        "TotalStudents": "N/A",
+        "ComputerRoom": "No",
+        "MobileNetwork": "No"
     },
     {
         "POTEAU_ID_POT": 60327,
@@ -93,42 +197,74 @@ const data = [
         "DESCRIPTION_RTP": "3- Fût",
         "Longitude": -73.58680,
         "Latitude": 45.628755,
-        "NOM_ARROND": "Rivière-des-Prairies - Pointe-aux-Trembles"
+        "NOM_ARROND": "Rivière-des-Prairies - Pointe-aux-Trembles",
+        "Latrine": "Yes",
+        "Township": "Libore",
+        "Status": "No",
+        "Electricity": "No",
+        "Internet": "No",
+        "ConstructionMaterial": "Mix",
+        "Water": "No",
+        "Fencing": "No",
+        "Library": "No",
+        "ConnectionSpeed": "N/A",
+        "Boys": "N/A",
+        "TeachingStaff": "N/A",
+        "PhoneNumber": "N/A",
+        "Girls": "N/A",
+        "TotalStudents": "N/A",
+        "ComputerRoom": "No",
+        "MobileNetwork": "No"
     }
     // Add more entries here...
 ];
 
-// Initialize the map and set its view to cover all valid data points
-const map = L.map('map');
+ // Initialize the map and set its view to the first point
+ const map = L.map('map').setView([data[0].Latitude, data[0].Longitude], 10);
 
-// Create a bounds object to include all valid data points
-const bounds = L.latLngBounds(data.map(item => [item.Latitude, item.Longitude]));
+ // Add a tile layer to the map
+ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+ }).addTo(map);
 
-// Fit the map to the bounds
-map.fitBounds(bounds);
-
-// Add a tile layer to the map
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-// Add markers to the map
-data.forEach(item => {
-    const marker = L.marker([item.Latitude, item.Longitude]).addTo(map);
-    const popupContent = `
-        <div class="popup-content">
-            <div class="popup-header">
-                <i class="fas fa-map-marker-alt"></i>
-                <h3>${item.NOM_ARROND}</h3>
-            </div>
-            <table class="popup-table">
-                <tr><td><i class="fas fa-map-marker-alt"></i> Latitude</td><td>${item.Latitude}</td></tr>
-                <tr><td><i class="fas fa-map-marker-alt"></i> Longitude</td><td>${item.Longitude}</td></tr>
-                <tr><td><i class="fas fa-globe"></i> Region</td><td>${item.NOM_ARROND}</td></tr>
-                <tr><td><i class="fas fa-map-signs"></i> Position</td><td>${item.POSITION_POP}</td></tr>
-                <tr><td><i class="fas fa-calendar-alt"></i> Date</td><td>${item.DATE_CONCEPTION_POT}</td></tr>
-                <tr><td><i class="fas fa-info-circle"></i> Description</td><td>${item.DESCRIPTION_RTP}</td></tr>
-            </table>
+ // Add markers to the map
+ data.forEach(item => {
+     const marker = L.marker([item.Latitude, item.Longitude]).addTo(map);
+     const popupContent = `
+         <div class="popup-content">
+             <div class="popup-header">
+                 <i class="fas fa-map-marker-alt"></i>
+                 <h3>${item.NOM_ARROND}</h3>
+             </div>
+             <table class="popup-table">
+                 <tr><td><i class="fas fa-map-marker-alt"></i> Latitude</td><td>${item.Latitude}</td></tr>
+                 <tr><td><i class="fas fa-map-marker-alt"></i> Longitude</td><td>${item.Longitude}</td></tr>
+                 <tr><td><i class="fas fa-globe"></i> Region</td><td>${item.NOM_ARROND}</td></tr>
+                 <tr><td><i class="fas fa-map-signs"></i> Position</td><td>${item.POSITION_POP}</td></tr>
+                 <tr><td><i class="fas fa-calendar-alt"></i> Date</td><td>${item.DATE_CONCEPTION_POT}</td></tr>
+                 <tr><td><i class="fas fa-info-circle"></i> Description</td><td>${item.DESCRIPTION_RTP}</td></tr>
+             </table>
+             <table class="popup-table">
+                 <tr><td><i class="fas fa-toilet"></i> Latrine</td><td>${item.Latrine}</td></tr>
+                 <tr><td><i class="fas fa-city"></i> Township</td><td>${item.Township}</td></tr>
+                 <tr><td><i class="fas fa-info-circle"></i> Status</td><td>${item.Status}</td></tr>
+                 <tr><td><i class="fas fa-bolt"></i> Electricity</td><td>${item.Electricity}</td></tr>
+                 <tr><td><i class="fas fa-wifi"></i> Internet</td><td>${item.Internet}</td></tr>
+                 <tr><td><i class="fas fa-building"></i> Construction Material</td><td>${item.ConstructionMaterial}</td></tr>
+                 <tr><td><i class="fas fa-water"></i> Water</td><td>${item.Water}</td></tr>
+             </table>
+             <table class="popup-table">
+                 <tr><td><i class="fas fa-fence"></i> Fencing</td><td>${item.Fencing}</td></tr>
+                 <tr><td><i class="fas fa-book"></i> Library</td><td>${item.Library}</td></tr>
+                 <tr><td><i class="fas fa-tachometer-alt"></i> Connection Speed</td><td>${item.ConnectionSpeed}</td></tr>
+                 <tr><td><i class="fas fa-male"></i> Boys</td><td>${item.Boys}</td></tr>
+                 <tr><td><i class="fas fa-chalkboard-teacher"></i> Teaching Staff</td><td>${item.TeachingStaff}</td></tr>
+                 <tr><td><i class="fas fa-phone"></i> Phone Number</td><td>${item.PhoneNumber}</td></tr>
+                 <tr><td><i class="fas fa-female"></i> Girls</td><td>${item.Girls}</td></tr>
+                 <tr><td><i class="fas fa-user-graduate"></i> Total Students</td><td>${item.TotalStudents}</td></tr>
+                 <tr><td><i class="fas fa-desktop"></i> Computer Room</td><td>${item.ComputerRoom}</td></tr>
+                 <tr><td><i class="fas fa-mobile-alt"></i> Mobile Network</td><td>${item.MobileNetwork}</td></tr>
+             </table>
         </div>
     `;
     marker.bindPopup(popupContent);
